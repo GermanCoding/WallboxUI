82c7399c2477e61bce61e41079b4725720ec2b8c
GermanCoding <github@germancoding.com>
2024-01-12 18:28:07 +0100

    Ship django's static files on the frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,6 +171,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '../frontend/django-static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
